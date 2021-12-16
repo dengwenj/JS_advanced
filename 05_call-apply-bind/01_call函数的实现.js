a8d25c4f4251763fd49848c1ce0fc157b0e91754
dengwenj <927561237@qq.com>
2021-12-16 21:44:49 +0800

    call函数实现

---
+++ 05_call-apply-bind/01_call函数的实现.js
@@ -0,0 +1,31 @@
+Function.prototype.wjcall = function (thisArg, ...args) {
+    // console.log(this);
+    var fn = this
+
+    // console.log(...args); // 数组可以这样，对象不行
+
+    // 对 thisArg 转为对象类型（防止传入的是非对象类型）
+    thisArg = thisArg ? Object(thisArg) : window
+
+    // 调用需要被执行的函数
+    thisArg.fn = fn
+    var res = thisArg.fn(...args)
+    delete thisArg.fn
+
+    return res
+}
+
+function foo() {
+    console.log(this);
+}
+
+foo.wjcall(123)
+
+function sum(num1, num2) {
+    console.log(this);
+    return num1 + num2
+}
+
+var res1 = sum.wjcall({name: 'dengwj'}, 20, 30)
+console.log(res1);
+
