a0fc556897f20345feccd30c4716b0239143e11c
dengwenj <927561237@qq.com>
2021-12-19 00:06:45 +0800

    创建对象的方式

---
+++ 07_面向对象（一）/01_创建对象.js
@@ -0,0 +1,12 @@
+// 1 通过 new object 创建
+var obj = new Object()
+obj.name = 'dengwj'
+obj.age = 21 
+console.log(obj);
+
+//  通过字面量创建
+var obj1 = {
+    name: 'dengwj',
+    age: 21
+}
+console.log(obj1);
