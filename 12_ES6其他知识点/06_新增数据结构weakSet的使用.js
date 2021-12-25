311742a82add68854f23a252ffa750dbc5af3000
dengwenj <927561237@qq.com>
2021-12-25 17:54:05 +0800

    weakSet的应用场景

--- 12_ES6其他知识点/06_新增数据结构weakSet的使用.js
+++ 12_ES6其他知识点/06_新增数据结构weakSet的使用.js
@@ -14,3 +14,21 @@ console.log(s); // 强引用
 const s1 = new WeakSet()
 s1.add(obj) // 对元素的引用是弱引用
 console.log(s1);
+
+
+// weakSet 的应用场景
+const personSet = new WeakSet()
+class Person {
+  constructor() {
+    personSet.add(this)
+  }
+  running() {
+    if (!personSet.has(this)) {
+      throw new Error('不能通过非构造方法创建出来的对象调用 running 方法')
+    }
+    console.log('在跑步', this);
+  }
+}
+
+const p = new Person()
+p.running.call({ name: 'dengwj' })
