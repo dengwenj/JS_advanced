585964342d97cd59758cf2b71fde724a5209f355
dengwenj <927561237@qq.com>
2021-12-25 21:50:51 +0800

    Object.values方法

---
+++ 14_ES8知识点/01_Object.values方法.js
@@ -0,0 +1,9 @@
+const obj = {
+  name: 'dengwj',
+  age: 21
+}
+
+console.log(Object.values(obj)); // [ 'dengwj', 21 ]
+
+console.log(Object.values([1, 2, 3])); // [ 1, 2, 3 ]
+console.log(Object.values('abc')); // [ 'a', 'b', 'c' ]
