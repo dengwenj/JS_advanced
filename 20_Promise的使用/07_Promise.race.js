692eaaa2123d2b79be87e9100f40d3248218fc6f
dengwenj <927561237@qq.com>
2021-12-27 23:07:42 +0800

    Promise.race

--- 20_Promise的使用/07_Promise.race.js
+++ 20_Promise的使用/07_Promise.race.js
@@ -8,7 +8,7 @@ const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     // resolve('p2')
     reject('error')
-  }, 2000);
+  }, 500);
 })
 
 const p3 = new Promise((resolve, reject) => {
@@ -17,14 +17,9 @@ const p3 = new Promise((resolve, reject) => {
   }, 3000);
 })
 
-Promise.allSettled([p1, p2, p3]).then((res) => { //  等到所有的都执行完才会有最终的状态，在 then 里面，不管是 fulfilled 还是 rejected
+// race 谁快用谁
+Promise.race([p1, p2, p3]).then((res) => {
   console.log(res);
-  // [
-  //   { status: 'fulfilled', value: 'p1' },
-  //   { status: 'rejected', reason: 'error' },
-  //   { status: 'fulfilled', value: 'p3' }
-  // ]  
 }).catch((err) => {
   console.log(err);
 })
-
