bb147ad8c4112fd53f69346cd4907307f8217b57
dengwenj <927561237@qq.com>
2021-12-28 00:14:42 +0800

    手写Promise-then方法设计

--- 20_Promise的使用/10_手写Promise-then方法设计.js
+++ 20_Promise的使用/10_手写Promise-then方法设计.js
@@ -10,23 +10,39 @@ class DWJPromise {
     const resolve = (value) => {
       if (this.state === PROMISE_STATUS_PENDING) {
         this.state = PROMISE_STATUS_FULFILLED
-        this.result = value
+        queueMicrotask(() => { // 会把放在微任务里面去
+          this.result = value
+          this.onFulfilled(this.result)
+        })
       }
     };
 
     const reject = (reason) => {
       if (this.state === PROMISE_STATUS_PENDING) {
         this.state = PROMISE_STATUS_REJECTED
-        this.result = reason
-        console.log('reject');
+        queueMicrotask(() => {
+          this.result = reason
+          this.onRejected(this.result)
+        })
       }
     };
 
     executor(resolve, reject)
   }
+
+  then(onFulfilled, onRejected) {
+    this.onFulfilled = onFulfilled
+    this.onRejected = onRejected
+  }
 }
 
-new DWJPromise((resolve, reject) => {
+const p = new DWJPromise((resolve, reject) => {
   resolve('11')
   reject('22')
 })
+
+p.then((res) => { // 这里 then 方法执行过后才去执行 reslove 函数，才会执行这个回调函数
+  console.log('res:', res);
+}, (err) => {
+  console.log('err:', err);
+})  
