93a8567f255c199374e8031374fb782e57cce77f
dengwenj <927561237@qq.com>
2021-12-30 22:18:10 +0800

    生成迭代器的函数

---
+++ 21_迭代器-生成器/02_生成迭代器的函数.js
@@ -0,0 +1,31 @@
+function createArrayIterator(arr) {
+  let index = 0
+  return {
+    next() {
+      if (index < arr.length) {
+        return { done: false, value: arr[index++] }
+      } else {
+        return { done: true, value: undefined }
+      }
+    }
+  }
+}
+
+const names = ['dwj', 'zww', 'lilei']
+const nums = [1, 2, 3, 4]
+ 
+const namesIterator = createArrayIterator(names)
+console.log(namesIterator.next())
+console.log(namesIterator.next())
+console.log(namesIterator.next())
+console.log(namesIterator.next())
+console.log(namesIterator.next())
+
+const numsIterator = createArrayIterator(nums)
+console.log(numsIterator.next());
+console.log(numsIterator.next());
+console.log(numsIterator.next());
+console.log(numsIterator.next());
+console.log(numsIterator.next());
+console.log(numsIterator.next());
+console.log(numsIterator.next());
