0316397fcac842d4afe95a44eb342ec700069c02
dengwenj <927561237@qq.com>
2022-01-02 23:47:55 +0800

    await后面的值

--- 22_async、await/02_async抛出异常.js
+++ 22_async、await/02_async抛出异常.js
@@ -1,6 +1,7 @@
 async function foo() {
   console.log('foo');
 
+  // 异步函数中的异常，会被作为异步函数返回的 Promise 的 reject 值的
   throw new Error('msg error')
   console.log('不会执行了');
 }
@@ -12,3 +13,24 @@ promise.catch((res) => {
 })
 
 console.log('后面执行');
+
+
+function getData() {
+  return new Promise((resolve, reject) => {
+    reject('reject')
+  })
+}
+
+async function foo1() {
+  try {
+    const res = await getData()
+  } catch (error) {
+    console.log(error, '错误的');
+  }
+
+  // console.log(res);
+}
+foo1()
+// foo1().catch((err) => {
+//   console.log(err); // 不用写捕获
+// })
