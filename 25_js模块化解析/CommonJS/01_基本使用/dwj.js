e92c53d55aa5399ae28e5b974ee9477a9fcef6cd
dengwenj <927561237@qq.com>
2022-01-03 23:03:22 +0800

    CommonJS基本使用

---
+++ 25_js模块化解析/CommonJS/01_基本使用/dwj.js
@@ -0,0 +1,12 @@
+const name = 'dengwenjie'
+const age = 21
+
+function sum(num1, num2) {
+  return num1 + num2
+}
+
+module.exports = {
+  name,
+  age,
+  sum
+}
---
+++ 25_js模块化解析/CommonJS/01_基本使用/main.js
@@ -0,0 +1,5 @@
+const { name, age, sum } = require('./dwj')
+
+console.log(name);
+console.log(age);
+console.log(sum(21, 16));
