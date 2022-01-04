3f25bd7993135984b0da71d4d2e9988fcb47c6ff
dengwenj <927561237@qq.com>
2022-01-04 22:56:23 +0800

    导入导出

--- 25_js模块化解析/ESModule/01_基本使用/foo.js
+++ 25_js模块化解析/ESModule/01_基本使用/foo.js
@@ -1,2 +1,33 @@
-export const name = 'dengwj'
-export const age = 21
+// 第一种方式：export 声明语句
+// export const name = 'dengwj'
+// export const age = 21
+
+// export function foo() {
+    
+// }
+
+// export class Person {
+     
+// }
+
+// 第二种方式 统一导出
+const name = 'dengwj'
+const age = 21
+function foo() {}
+class Person {}
+
+// 这不是对象，这是一种语法，不能写键值对，统一导出
+export {
+  name,
+  age,
+  foo,
+  Person
+}
+
+// 第三种方式，其别名 as
+// export {
+//   name as name1,
+//   age as age1,
+//   foo as foo1,
+//   Person as Person1
+// }
--- 25_js模块化解析/ESModule/01_基本使用/main.js
+++ 25_js模块化解析/ESModule/01_基本使用/main.js
@@ -1,4 +1,20 @@
-import { name, age } from './foo.js'
+// 导入方式1 ：普通的方式
+// import { name, age } from './foo.js'
+// import { name1, age1, foo1, Person1 } from './foo.js'
 
-console.log(name);
-console.log(age);
+// 导入方式2: 其别名
+// import { name as name1,  age as age1, foo as foo1, Person as Person1} from './foo.js'
+
+// 导入方式3: 将导出的所有内容放到一个标识符中
+import * as foo from './foo.js'
+
+// console.log(name);
+// console.log(age);
+
+// console.log(name1);
+// console.log(age1);
+// console.log(foo1);
+// console.log(Person1);
+
+console.log(foo.name);
+console.log(foo.age);
