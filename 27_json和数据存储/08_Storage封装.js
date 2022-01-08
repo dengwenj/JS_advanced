67a243b8cd0e32c2aca9cfee1a2476ec4fa40694
dengwenj <927561237@qq.com>
2022-01-08 15:24:01 +0800

    storage封装

---
+++ 27_json和数据存储/08_Storage封装.js
@@ -0,0 +1,43 @@
+class DWJCache {
+  constructor(isLocal = true) {
+    this.storage = isLocal ? localStorage : sessionStorage
+  }
+
+  setItem(key, value) {
+    if (value) {
+      this.storage.setItem(key, JSON.stringify(value))
+    }
+  }
+
+  getItem(key) {
+    let value = this.storage.getItem(key)
+    if (value) {
+      value = JSON.parse(value)
+      return value
+    }
+  }
+
+  removeItem(key) {
+    this.storage.removeItem(key)
+  }
+
+  clear() {
+    this.storage.clear()
+  }
+
+  key(index) {
+    return this.storage.key(index)
+  }
+
+  length() {
+    return this.storage.length 
+  }
+}
+
+const localStorage = new DWJCache()
+const sessionStorage = new DWJCache(false)
+
+export {
+  localStorage,
+  sessionStorage
+}
