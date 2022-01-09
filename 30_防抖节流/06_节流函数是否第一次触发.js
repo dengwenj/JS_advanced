e2576c94755c8f939f00a8c10137b0ea663f35f1
dengwenj <927561237@qq.com>
2022-01-09 20:52:32 +0800

    是否第一次触发

--- 30_防抖节流/06_节流函数是否第一次触发.js
+++ 30_防抖节流/06_节流函数是否第一次触发.js
@@ -1,26 +1,20 @@
-function throttle(fn, interval) {
+function throttle(fn, interval, options = { leading: true, trailing: false }) {
+  const { leading, trailing } = options
   let lastTime = 0
-  // let is = true
-  // let timer = null
 
   const _throttle = function () {
     // 获取当前事件触发的时间
     const nowTime = new Date().getTime()
+
+    // 是否第一次触发
+    if (!lastTime && !leading) lastTime = nowTime
+
     const remainTime = interval - (nowTime - lastTime)
 
     if (remainTime <= 0) {
       fn()
       lastTime = nowTime
     }
-
-    // if (is) {
-    //   is = false
-    //   timer = setTimeout(() => {
-    //     fn()
-    //     is = true
-    //     timer = null
-    //   }, interval)
-    // }
   }
 
   return _throttle
