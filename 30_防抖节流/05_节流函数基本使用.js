0f67a0aeebe0337d2f74f1006de470e8cb0e835c
dengwenj <927561237@qq.com>
2022-01-09 18:56:36 +0800

    节流函数档基本实现

---
+++ 30_防抖节流/05_节流函数基本使用.js
@@ -0,0 +1,26 @@
+function throttle(fn, interval) {
+  let lastTime = 0
+  // let is = true
+  // let timer = null
+
+  const _throttle = function () {
+    const nowTime = new Date().getTime()
+    const remainTime = interval - (nowTime - lastTime)
+
+    if (remainTime <= 0) {
+      fn()
+      lastTime = nowTime
+    }
+
+    // if (is) {
+    //   is = false
+    //   timer = setTimeout(() => {
+    //     fn()
+    //     is = true
+    //     timer = null
+    //   }, interval)
+    // }
+  }
+
+  return _throttle
+}
