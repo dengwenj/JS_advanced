613083f5d24429e4c5f72e225452be854f6af6c0
dengwenj <927561237@qq.com>
2022-01-10 00:48:16 +0800

    简单手写事件总线

---
+++ 31_深拷贝-事件总线/03_自定义事件总线.js
@@ -0,0 +1,37 @@
+class DWJEventBus {
+  constructor() {
+    this.eventBus = {}
+  }
+
+  on(eventName, eventCallback, thisArg) {
+    let handlers = this.eventBus[eventName]
+    if (!handlers) {
+      handlers = []
+      this.eventBus[eventName] = handlers
+    }
+    handlers.push({
+      eventCallback,
+      thisArg
+    })
+  }
+
+  emit(eventName, ...payload) {
+    this.eventBus[eventName].forEach(item => {
+      item.eventCallback.call(item.thisArg, ...payload)
+    })
+  }
+
+  off() {
+
+  }
+}
+
+const eventBus = new DWJEventBus()
+
+eventBus.on('dwj', function (num, str) {
+  console.log(num, str, this);
+}, { name: 'dngwj' })
+
+eventBus.emit('dwj', 123, 'zww')
+eventBus.emit('dwj', 123333333333, 'zww')
+
