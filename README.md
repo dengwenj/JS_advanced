505fea856a2625f7779470135fcaad697a778dd7
dengwenj <927561237@qq.com>
2022-05-28 23:25:17 +0800

    修改

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaScript 高级
+## 深入学习 JavaScript 没事就重学
+
 深入理解 JavaScript
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log(111)
+const dwj = 'dengwenjie'
+const ww = 'ww'
+let dwjWw
+
+for (let i = 0; i < 10; i++) {
+  dwjWw = dwjWw || ''
+
+  dwjWw += `${dwj} ❤️ ${ww}`
+  console.log(dwjWw)
+}
