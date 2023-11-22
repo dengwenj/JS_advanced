6ae08d537ce8cd641b65d07148f0521522260f09
朴睦 <927561237@qq.com>
2023-11-22 20:33:33 +0800

    函数执行上下文栈

--- js2/02_函数执行-作用域链-面试题-内存管理/01_全局函数的执行过程.js
+++ js2/02_函数执行-作用域链-面试题-内存管理/01_全局函数的执行过程.js
@@ -2,7 +2,27 @@ var name = '朴睦'
 
 // foo 可以在前面执行, 因为在编译解析的时候会创建 foo 存储函数对象，{ foo: 0x123 } foo 指向这个函数内存
 // 在执行的时候就有 foo 就可以调用
-foo()
-function foo() {
-  console.log("我是函数");
+// foo()
+// function foo() {
+//   console.log("我是函数");
+// }
+
+var age = 23
+function bar() {
+  async function ww() {
+    var sex = "男"
+    console.log(sex)
+
+    const res = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("await")
+      }, 1000)
+    })
+    console.log(res)
+  }
+  ww()
+
+  console.log(age)
 }
+bar()
+// 打印 男 23 await
