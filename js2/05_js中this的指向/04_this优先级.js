4729a11ff59543f3fad5b81ed609fadac550acea
朴睦 <927561237@qq.com>
2023-11-25 17:13:28 +0800

    箭头函数的特性

--- js2/05_js中this的指向/04_this优先级.js
+++ js2/05_js中this的指向/04_this优先级.js
@@ -17,3 +17,16 @@ var ww = bar.bind("dwj")
 var obj = new ww()
 
 // new绑定 > 显示绑定(apply/call/bind) > 隐式绑定(obj.foo()) > 默认绑定(独立函数调用)
+// bind > bind、apply
+
+function fn() {
+  console.log(this) // 全局对象
+}
+fn.apply(null)
+fn.apply(undefined) 
+var f = fn.bind(null)
+
+/**
+ * 箭头函数不会绑定 this、arguments 属性
+ * 箭头函数不能作为构造函数来使用(不能和 new 一起来使用,会抛出错误)
+ */
