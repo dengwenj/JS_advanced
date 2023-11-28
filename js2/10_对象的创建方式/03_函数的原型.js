36d333dad5ac4f05282a6083b8151ada1154dea1
朴睦 <927561237@qq.com>
2023-11-28 22:14:44 +0800

    函数的原型

---
+++ js2/10_对象的创建方式/03_函数的原型.js
@@ -0,0 +1,15 @@
+function Person() {
+  
+}
+const p1 = new Person()
+const p2 = new Person()
+
+console.log(p1.__proto__ === Person.prototype) // true
+console.log(p1.__proto__ === Person.prototype) // true
+console.log(p2.__proto__ === p1.__proto__) // true 
+
+/**
+ * 函数也是一个对象，也有__proto__
+ * 函数它因为是一个函数，所以它还会多出来一个显式原型属性: prototype
+ * 对象的隐式原型 __proto__ 会指向构造函数的显式原型 prototype 
+ */
